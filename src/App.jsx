ecb0321b66de360056a00d98e992cc87445e8760
Ali Albabily <32642133+AliAlbabily@users.noreply.github.com>
2025-10-22 22:43:22 +0200

    New app colors

--- src/App.jsx
+++ src/App.jsx
@@ -99,15 +99,15 @@ const App = () => {
                                         <button className="icon-btn" onClick={() => openEditModal(ex.id)} title="Edit">
                                             {/* pencil icon */}
                                             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25z" fill="#333"/>
-                                                <path d="M20.71 7.04a1.003 1.003 0 0 0 0-1.42l-2.34-2.34a1.003 1.003 0 0 0-1.42 0l-1.83 1.83 3.75 3.75 1.84-1.82z" fill="#333"/>
+                                                <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25z" fill="#222"/>
+                                                <path d="M20.71 7.04a1.003 1.003 0 0 0 0-1.42l-2.34-2.34a1.003 1.003 0 0 0-1.42 0l-1.83 1.83 3.75 3.75 1.84-1.82z" fill="#222"/>
                                             </svg>
                                         </button>
                                         <button className="icon-btn" onClick={() => deleteExercise(ex.id)} title="Delete">
                                             {/* trash icon */}
                                             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6v12z" fill="#c00"/>
-                                                <path d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" fill="#c00"/>
+                                                <path d="M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6v12z" fill="#cb5555"/>
+                                                <path d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" fill="#cb5555"/>
                                             </svg>
                                         </button>
                                     </td>
